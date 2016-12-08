8ef67e3e798f44d12f6c2cb0d5686871b845b10d
Brian Hendel <brian.hendel@gmail.com>
2016-12-08 15:51:12 -0500

    Links Angular and Rails, up to the Devise step

--- angular-rails-reddit/app/assets/javascripts/home/mainCtrl.js
+++ angular-rails-reddit/app/assets/javascripts/home/mainCtrl.js
@@ -8,20 +8,15 @@ angular.module('flapperNews')
 
 			$scope.addPost = function () {
 				if (!$scope.title || $scope.title === "") { return; }
-				$scope.posts.push({
+				posts.create({
 					title: $scope.title,
 					link: $scope.link,
-					upvotes: 0,
-					comments: [
-						{ author: "David", body: "Cool post!", upvotes: 0 },
-						{ author: "Chrisopher", body: "I like it!", upvotes: 0 }
-					]
 				});
 				$scope.title = "";
 				$scope.link = "";
 			};
 
 			$scope.incrementUpvotes = function (post) {
-				post.upvotes += 1;
+				posts.upvote(post);
 			};
 		}])
